ea9ff354c034c8c211209bd0e892309f8c41837d
Logan Karney <karneyl@mail.gvsu>
2019-12-19 14:06:38 -0500

    Added basic input handlers

--- player/src/Login.jsx
+++ player/src/Login.jsx
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import './Login.css'
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userEmail: "",
+            userPassword: ""
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Login</h2>
                 <div id="container">
-                    <label className=" label email-label">Email</label>
-                    <input type="text" className="field email-field" />
+                    <label className="label email-label">Email</label>
+                    <input name="userEmail" type="text" value={this.state.userEmail} onChange={(e) => this.updateFormData(e)} className="field email-field" />
                     <label className="label pass-label">Password</label>
-                    <input type="password" className="field pass-field" />
-                    <button className="button signup-button">SignUp</button>
-                    <button className="button signin-button">SignIn</button>
+                    <input name="userPassword" type="password" value={this.state.userPassword} onChange={(e) => this.updateFormData(e)} className="field pass-field" />
+                    <button onClick={() => this.doSignUp()} className="button signup-button">SignUp</button>
+                    <button onClick={() => this.doSignIn()} className="button signin-button">SignIn</button>
                 </div>
             </div>
         );
+    }
+
+    doSignUp() {
+        alert(`Signed up ${this.state.userEmail}`)
+    }
+
+    doSignIn() {
+        alert(`Signed in ${this.state.userEmail}`)
+    }
+
+    updateFormData(ev) {
+        if (ev.target.type === 'number')
+            this.setState({ [ev.target.name]: Number(ev.target.value) });
+        else
+            this.setState({ [ev.target.name]: ev.target.value });
     }
 }
 

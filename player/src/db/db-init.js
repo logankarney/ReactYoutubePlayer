cdde532ef1ed47bfee46558c8830cb923e73bf87
Logan Karney <karneyl@mail.gvsu>
2019-12-19 14:39:22 -0500

    *Removed Storage

--- player/src/db/db-init.js
+++ player/src/db/db-init.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
-import "firebase/storage";
 
 // Create this file using information from your FireBase Realtime DB app
 var firebaseConfig = require("./db-config");
@@ -11,6 +10,5 @@ firebase.initializeApp(firebaseConfig);
 
 const AppDB = firebase.database();
 const AppAUTH = firebase.auth();
-const AppStorage = firebase.storage();
 
-export { AppDB, AppAUTH, AppStorage };
+export { AppDB, AppAUTH };

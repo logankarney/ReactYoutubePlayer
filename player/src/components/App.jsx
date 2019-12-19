e54cc58604f6a66b2dd29cc3f567510590ede293
Logan Karney <karneyl@mail.gvsu>
2019-12-19 14:56:05 -0500

    *Started adding routing

--- player/src/components/App.jsx
+++ player/src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Login from "./Login";
 import Selection from "./Selection";
 import Add from "./Add";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "../css/App.css";
 
 function App() {
   return (
     <div className="App App-header">
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route path="/selection" component={Selection} />
+          <Route path="/add" component={Add} />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }

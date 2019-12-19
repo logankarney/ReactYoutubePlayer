bcc17110794ac61fe248174bebf23148a45cfaa8
Logan Karney <karneyl@mail.gvsu>
2019-12-19 13:56:24 -0500

    *Added css

--- player/src/Login.jsx
+++ player/src/Login.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import './Login.css'
 
 class Login extends Component {
     render() {
         return (
             <div>
                 <h2>Login</h2>
-                <div>
-                    <label>Email</label>
-                    <input type="text" />
-                    <label>Password</label>
-                    <input type="password" />
-                    <div>
-                        <button>SignUp</button>
-                        <button>SignIn</button>
-                    </div>
+                <div id="container">
+                    <label className=" label email-label">Email</label>
+                    <input type="text" className="field email-field" />
+                    <label className="label pass-label">Password</label>
+                    <input type="password" className="field pass-field" />
+                    <button className="button signup-button">SignUp</button>
+                    <button className="button signin-button">SignIn</button>
                 </div>
             </div>
         );

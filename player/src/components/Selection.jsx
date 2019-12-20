ee33a208700aaaefc37d229347daec466e36a4b5
Logan Karney <karneyl@mail.gvsu>
2019-12-20 11:57:22 -0500

    *Added (hardcoded) table

--- player/src/components/Selection.jsx
+++ player/src/components/Selection.jsx
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
 
+import { AppDB } from "../db/db-init";
+
 class Selection extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userEmail: this.props.location.state.userEmail,
+            rooms: []
+        }
+    }
+
+    componentDidMount() {
+        AppDB.ref("Rooms").on('child_added', (s) => this.roomAddListener(s));
+        AppDB.ref("Rooms").on("child_removed", (s) => this.roomRemoveListener(s));
+    }
+
     render() {
         return (
             <div>
                 <h2>Available Rooms</h2>
-                <div>
-                    <select>
-                        <option value="room1">Room1</option>
-                    </select>
-                </div>
+                <button>Create</button>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Owner</td>
+                            <td>Persons</td>
+                            <td>Password</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Test</td>
+                            <td>UserName</td>
+                            <td>5/9</td>
+                            <td>No</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </div>
         );
+    }
+
+    roomAddListener(snapshot) {
+        const item = snapshot.val();
+
+        const newRooms = this.state.rooms.slice(); /* creates a copy */
+        newRooms.push({ ...item, mykey: snapshot.key }); /* adds a new item */
+
+        this.setState({ rooms: newRooms });
+    }
+
+    roomRemoveListener(snapshot) {
+        /* snapshot.key will hold the key of the item being REMOVED */
+        const newRooms = this.state.rooms.slice().filter(z => z.mykey !== snapshot.key);
+        this.setState({ rooms: newRooms });
     }
 }
 
